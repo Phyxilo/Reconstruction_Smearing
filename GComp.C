17894ddc29710e543092441de34828b786d09e14
Phyxilo <eminyuksel97@gmail.com>
2022-05-02 23:07:04 +0300

    Second commit

--- GComp.C
+++ GComp.C
@@ -10,210 +10,180 @@
 
 using namespace std;
 
-void dataRead();
+void HistDrawMC(TFile *file, TH1F *TXHist, TH1F *TYHist, TH1F *T2Hist);
+void HistDrawData(TFile *file, TH1F *TXHist, TH1F *TYHist, TH1F *T2Hist);
+void HistFormat(TH1F *Hist0, TH1F *Hist1);
+void HistFormatMulti(TH1F *Hist0, TH1F *Hist1, TH1F *Hist2);
+
+TCanvas *Canvas= new TCanvas("Canvas","Histogram Canvas",20,20,1920,1080);
+
+TH1F *eTXDataHist = new TH1F("eTXDataHist","Slope X Histogram ",50,-0.5,0.5);
+TH1F *eTYDataHist = new TH1F("eTYDataHist","Slope Y Histogram ",50,-0.5,0.5);
+
+TH1F *eTXMC0Hist = new TH1F("eTXMCHist","Slope X Histogram ",50,-0.5,0.5);
+TH1F *eTYMC0Hist = new TH1F("eTYMCHist","Slope Y Histogram ",50,-0.5,0.5);
+TH1F *eTXMC1Hist = new TH1F("eTXMCHist","Slope X Histogram ",50,-0.5,0.5);
+TH1F *eTYMC1Hist = new TH1F("eTYMCHist","Slope Y Histogram ",50,-0.5,0.5);
+
+TH1F *eT2DataHist = new TH1F("eTXDataHist","Space Angle Histogram ",50,0,1);
+TH1F *eT2MC0Hist = new TH1F("eTXDataHist","Space Angle Histogram ",50,0,1);
+TH1F *eT2MC1Hist = new TH1F("eTXDataHist","Space Angle Histogram ",50,0,1);
 
 void GComp()
 {
-  dataRead();
-}
+  char dataName[64], MC0Name[64], MC1Name[64];
+  char outName[64], outNameBegin[64], outNameEnd[64];
 
-void dataRead()
-{
-  char dataName[64], MCName[64], MCLeafName[64];
-  //sprintf( dataName, "~/Downloads/data_finealign/%02d_001.cp.root", index);
-  //sprintf( MCName, "~/Downloads/cp_data_pl001_105/%02d_001.cp.root", index);
-  //sprintf( MCLeafName, "couples%d", index);
-
-  sprintf(MCName, "/home/phyxilo/Downloads/pl001_030/linked_tracks.root");
+  sprintf(MC0Name, "/home/phyxilo/Downloads/pl001_030/linked_tracks.root");
+  sprintf(MC1Name, "/home/phyxilo/Downloads/fedraOut/Out/finalCut.root");
   sprintf(dataName, "/home/phyxilo/Downloads/dataFile/data.root");
 
-  TCanvas *Canvas= new TCanvas("Canvas","Histogram Canvas",20,20,1920,1080);
-	Canvas->SetWindowSize(1920, 1080);
-	Canvas->SetCanvasSize(192*6, 108*6);
+  sprintf(outName, "histogram.pdf");
+  sprintf(outNameBegin, "%s[", outName);
+  sprintf(outNameEnd, "%s]", outName);
+
+  Canvas->SetWindowSize(1920, 1080);
+  Canvas->SetCanvasSize(192*6, 108*6);
 
   TFile *data = TFile::Open(dataName);
-  TFile *monteCarlo = TFile::Open(MCName);
+  //TFile *monteCarlo0 = TFile::Open(MC0Name);
+  TFile *monteCarlo1 = TFile::Open(MC1Name);
 
-  TH1F *eXDataHist = new TH1F("eXDataHist","Data Histogram ",100,-15000,15000);
-  TH1F *eYDataHist = new TH1F("eYDataHist","Data Histogram ",100,-15000,15000);
-  TH1F *eTXDataHist = new TH1F("eTXDataHist","Slope X Histogram ",100,-0.5,0.5);
-  TH1F *eTYDataHist = new TH1F("eTYDataHist","Slope Y Histogram ",100,-0.5,0.5);
-
-  TH1F *eXMCHist = new TH1F("eXMCHist","MonteCarlo Histogram ",100,-15000,15000);
-  TH1F *eYMCHist = new TH1F("eYMCHist","MonteCarlo Histogram ",100,-15000,15000);
-  TH1F *eTXMCHist = new TH1F("eTXMCHist","Slope X Histogram ",100,-0.5,0.5);
-  TH1F *eTYMCHist = new TH1F("eTYMCHist","Slope Y Histogram ",100,-0.5,0.5);
-
-  TH1F *eT2DataHist = new TH1F("eTXDataHist","Space Angle Histogram ",100,0,1);
-  TH1F *eT2MCHist = new TH1F("eTXDataHist","Space Angle Histogram ",100,0,1);
-
-  //TTree *dataTree = (TTree*)data->Get("couples");
-  //TTree *MCTree = (TTree*)monteCarlo->Get(MCLeafName);
   TTree *dataTree = (TTree*)data->Get("TRK");
-  TTree *MCTree = (TTree*)monteCarlo->Get("tracks");
+  //TTree *MC0Tree = (TTree*)monteCarlo0->Get("tracks");
+  TTree *MC1Tree = (TTree*)monteCarlo1->Get("tracks");
 
-  //TLeaf *eXData = dataTree->GetLeaf("t.eX");
-  //TLeaf *eYData = dataTree->GetLeaf("t.eY");
-  TLeaf *eTXData = dataTree->GetLeaf("tx");
-  TLeaf *eTYData = dataTree->GetLeaf("ty");
+  //HistDrawMC(monteCarlo0, eTXMC0Hist, eTYMC0Hist, eT2MC0Hist);
+  HistDrawMC(monteCarlo1, eTXMC1Hist, eTYMC1Hist, eT2MC1Hist);
+  HistDrawData(data, eTXDataHist, eTYDataHist, eT2DataHist);
 
-  TLeaf *beamAngleX = dataTree->GetLeaf("txpeak");
-  TLeaf *beamAngleY = dataTree->GetLeaf("typeak");
+  Canvas->Print( outNameBegin,"pdf");
+  
+  HistFormat(eTXDataHist, eTXMC1Hist); Canvas->Print( outName,"pdf");
+  HistFormat(eTYDataHist, eTYMC1Hist); Canvas->Print( outName,"pdf");
+  HistFormat(eT2DataHist, eT2MC1Hist); Canvas->Print( outName,"pdf");
+  
+  /*
+  HistFormatMulti(eTXDataHist, eTXMC0Hist, eTXMC1Hist); Canvas->Print( outName,"pdf");
+  HistFormatMulti(eTYDataHist, eTYMC0Hist, eTYMC1Hist); Canvas->Print( outName,"pdf");
+  HistFormatMulti(eT2DataHist, eT2MC0Hist, eT2MC1Hist); Canvas->Print( outName,"pdf");
+  */
+  Canvas->Print( outNameEnd,"pdf");
+}
 
-  //TLeaf *eXMC = MCTree->GetLeaf("t.eX");
-  //TLeaf *eYMC = MCTree->GetLeaf("t.eY");
-  TLeaf *eTXMC = MCTree->GetLeaf("t.eTX");
-  TLeaf *eTYMC = MCTree->GetLeaf("t.eTY");
+void HistDrawMC(TFile *file, TH1F *TXHist, TH1F *TYHist, TH1F *T2Hist)
+{
+  TTree *tree = (TTree*)file->Get("tracks");
 
-  TLeaf *MCPlateID = MCTree->GetLeaf("t.ePID");
+  TLeaf *eTX = tree->GetLeaf("t.eTX");
+  TLeaf *eTY = tree->GetLeaf("t.eTY");
 
-  Canvas->Print( "histogram.pdf[","pdf");
+  TLeaf *PlateID = tree->GetLeaf("t.ePID");
 
-  double dataBfDataX = 0;
-  double dataBfDataY = 0;
+  double dataBfX = 0;
+  double dataBfY = 0;
 
-  for (int i = 0; i < dataTree->GetEntriesFast(); i++)
+  for (int i = 0; i < tree->GetEntriesFast(); i++)
   {
-    dataTree->GetEntry(i);
+    tree->GetEntry(i);
 
-    //eXDataHist->Fill(eXData->GetValue());
-    //eYDataHist->Fill(eYData->GetValue());
-
-    double dataBfX = eTXData->GetValue() - beamAngleX->GetValue();
-    double dataBfY = eTYData->GetValue() - beamAngleY->GetValue();
-
-    eTXDataHist->Fill(dataBfX);
-    eTYDataHist->Fill(dataBfY);
-    eT2DataHist->Fill(sqrt(dataBfX*dataBfX + dataBfY*dataBfY));
-  }
-
-  double dataBfMCX = 0;
-  double dataBfMCY = 0;
-
-  for (int i = 0; i < MCTree->GetEntriesFast(); i++)
-  {
-    MCTree->GetEntry(i);
-
-    if (MCPlateID->GetValue() >= 1)
+    if (PlateID->GetValue() >= 1)
     {
       //eXMCHist->Fill(eXMC->GetValue());
       //eYMCHist->Fill(eYMC->GetValue());
 
-      dataBfMCX = eTXMC->GetValue();
-      dataBfMCY = eTYMC->GetValue();
+      dataBfX = eTX->GetValue();
+      dataBfY = eTY->GetValue();
 
-      eTXMCHist->Fill(dataBfMCX);
-      eTYMCHist->Fill(dataBfMCY);
-      eT2MCHist->Fill(sqrt(dataBfMCX*dataBfMCX + dataBfMCY*dataBfMCY));
+      TXHist->Fill(dataBfX);
+      TYHist->Fill(dataBfY);
+      T2Hist->Fill(sqrt(dataBfX*dataBfX + dataBfY*dataBfY));
     }
   }
+}
 
-  //eTYMCHist->SetDefaultBufferSize();
+void HistDrawData(TFile *file, TH1F *TXHist, TH1F *TYHist, TH1F *T2Hist)
+{
+  TTree *tree = (TTree*)file->Get("TRK");
 
-  /*
-  eXMCHist->Draw(); eXMCHist->SetLineColor(kBlue);
-  eXDataHist->Draw("SAMESE1"); eXDataHist->SetLineColor(kRed);
+  TLeaf *eTX = tree->GetLeaf("tx");
+  TLeaf *eTY = tree->GetLeaf("ty");
 
-  gStyle->SetOptFit();
-  Canvas->Update();
-  TPaveStats *peX = (TPaveStats*)eXMCHist->FindObject("stats");
-  peX->SetY1NDC(0.05);
-  peX->SetY2NDC(0.25);
-  Canvas->Modified();
+  TLeaf *beamAngleX = tree->GetLeaf("txpeak");
+  TLeaf *beamAngleY = tree->GetLeaf("typeak");
 
-  auto legendX = new TLegend(0.1, 0.8, 0.32, 0.9);
-  legendX->SetHeader("Histogram Legend","C");
-  legendX->AddEntry(eXDataHist,"Data","f");
-  legendX->AddEntry(eXMCHist,"MonteCarlo","f");
-  legendX->Draw();
+  double dataBfX = 0;
+  double dataBfY = 0;
 
-  Canvas->Print( "histogram.pdf","pdf");
+  for (int i = 0; i < tree->GetEntriesFast(); i++)
+  {
+    tree->GetEntry(i);
 
-  eYMCHist->Draw(); eYMCHist->SetLineColor(kBlue);
-  eYDataHist->Draw("SAMESE1"); eYDataHist->SetLineColor(kRed);
+    //eXDataHist->Fill(eXData->GetValue());
+    //eYDataHist->Fill(eYData->GetValue());
 
-  gStyle->SetOptFit();
-  Canvas->Update();
-  TPaveStats *peY = (TPaveStats*)eYMCHist->FindObject("stats");
-  peY->SetY1NDC(0.05);
-  peY->SetY2NDC(0.25);
-  Canvas->Modified();
+    double dataBfX = eTX->GetValue() - beamAngleX->GetValue();
+    double dataBfY = eTY->GetValue() - beamAngleY->GetValue();
 
-  auto legendY = new TLegend(0.1, 0.8, 0.32, 0.9);
-  legendY->SetHeader("Histogram Legend","C");
-  legendY->AddEntry(eYDataHist,"Data","f");
-  legendY->AddEntry(eYMCHist,"MonteCarlo","f");
-  legendY->Draw();
+    TXHist->Fill(dataBfX);
+    TYHist->Fill(dataBfY);
+    T2Hist->Fill(sqrt(dataBfX*dataBfX + dataBfY*dataBfY));
+  }
+}
 
-  Canvas->Print( "histogram.pdf","pdf");
-  */
+void HistFormat(TH1F *Hist0, TH1F *Hist1)
+{
 
-  eTXMCHist->Scale(eTXDataHist->Integral()/eTXMCHist->Integral());
+  Hist1->Scale(Hist0->Integral()/Hist1->Integral());
 
-  //eTXMCHist->Scale(1./eTYMCHist->Integral());
-  //eTXDataHist->Scale(1./eTYDataHist->Integral());
+  //Hist0->Scale(1./Hist0->Integral());
+  //Hist1->Scale(1./Hist1->Integral());
 
-  eTXMCHist->Draw("HIST"); eTXDataHist->SetLineColor(kRed);
-  eTXDataHist->Draw("SAMESE1"); eTXMCHist->SetLineColor(kBlue);
+  Hist1->GetYaxis()->SetRangeUser(0, 150000);
+
+  Hist1->Draw("HIST"); Hist1->SetLineColor(kBlue);
+  Hist0->Draw("SAMESE1"); Hist0->SetLineColor(kRed);
 
   gStyle->SetOptFit();
   Canvas->Update();
-  TPaveStats *peTX = (TPaveStats*)eTXMCHist->FindObject("stats");
+  TPaveStats *peTX = (TPaveStats*)Hist1->FindObject("stats");
   peTX->SetY1NDC(0.05);
   peTX->SetY2NDC(0.25);
   Canvas->Modified();
 
   auto legendTX = new TLegend(0.1, 0.8, 0.32, 0.9);
   legendTX->SetHeader("Histogram Legend","C");
-  legendTX->AddEntry(eTXDataHist,"Data","f");
-  legendTX->AddEntry(eTXMCHist,"MonteCarlo","f");
+  legendTX->AddEntry(Hist0,"Data","f");
+  legendTX->AddEntry(Hist1,"MonteCarlo","f");
   legendTX->Draw();
+}
 
-  Canvas->Print( "histogram.pdf","pdf");
+void HistFormatMulti(TH1F *Hist0, TH1F *Hist1, TH1F *Hist2)
+{
 
-  eTYMCHist->Scale(eTYDataHist->Integral()/eTYMCHist->Integral());
+  Hist1->Scale(Hist0->Integral()/Hist1->Integral());
+  Hist2->Scale(Hist0->Integral()/Hist2->Integral() + 0.2);
 
-  //eTYMCHist->Scale(1./eTYMCHist->Integral());
-  //eTYDataHist->Scale(1./eTYDataHist->Integral());
+  //Hist0->Scale(1./Hist0->Integral());
+  //Hist1->Scale(1./Hist1->Integral());
 
-  eTYMCHist->Draw("HIST"); eTYDataHist->SetLineColor(kRed);
-  eTYDataHist->Draw("SAMESE1"); eTYMCHist->SetLineColor(kBlue);
+  Hist0->GetYaxis()->SetRangeUser(0, 180000);
 
+  Hist0->Draw("HIST"); Hist0->SetLineColor(kBlue);
+  Hist1->Draw("SAMESE1"); Hist1->SetLineColor(kRed);
+  Hist2->Draw("SAMESE1"); Hist2->SetLineColor(kGreen);
+  /*
   gStyle->SetOptFit();
   Canvas->Update();
-  TPaveStats *peTY = (TPaveStats*)eTYMCHist->FindObject("stats");
-  peTY->SetY1NDC(0.05);
-  peTY->SetY2NDC(0.25);
+  TPaveStats *peTX = (TPaveStats*)Hist0->FindObject("stats");
+  peTX->SetY1NDC(0.05);
+  peTX->SetY2NDC(0.25);
   Canvas->Modified();
-
-  auto legendTY = new TLegend(0.1, 0.8, 0.32, 0.9);
-  legendTY->SetHeader("Histogram Legend","C");
-  legendTY->AddEntry(eTYDataHist,"Data","f");
-  legendTY->AddEntry(eTYMCHist,"MonteCarlo","f");
-  legendTY->Draw();
-
-  Canvas->Print( "histogram.pdf","pdf");
-
-  eT2MCHist->Scale(eT2DataHist->Integral()/eT2MCHist->Integral());
-
-  eT2MCHist->Draw("HIST"); eT2DataHist->SetLineColor(kRed);
-  eT2DataHist->Draw("SAMESE1"); eT2MCHist->SetLineColor(kBlue);
-
-  gStyle->SetOptFit();
-  Canvas->Update();
-  TPaveStats *peT2 = (TPaveStats*)eT2MCHist->FindObject("stats");
-  peT2->SetY1NDC(0.05);
-  peT2->SetY2NDC(0.25);
-  Canvas->Modified();
-
-  auto legendT2 = new TLegend(0.1, 0.8, 0.32, 0.9);
-  legendT2->SetHeader("Histogram Legend","C");
-  legendT2->AddEntry(eT2DataHist,"Data","f");
-  legendT2->AddEntry(eT2MCHist,"MonteCarlo","f");
-  legendT2->Draw();
-
-  Canvas->Print( "histogram.pdf","pdf");
-
-  Canvas->Print( "histogram.pdf]","pdf");
-
+  */
+  auto legendTX = new TLegend(0.1, 0.8, 0.32, 0.9);
+  legendTX->SetHeader("Histogram Legend","C");
+  legendTX->AddEntry(Hist2,"Data","f");
+  legendTX->AddEntry(Hist1,"Data","f");
+  legendTX->AddEntry(Hist0,"MonteCarlo","f");
+  legendTX->Draw();
 }
