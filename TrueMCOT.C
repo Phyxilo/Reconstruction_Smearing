a6555a565dfbff4134ffcc840b42ff7bb9a14060
Phyxilo <eminyuksel97@gmail.com>
2022-05-20 15:23:27 +0300

    TrueMCOT.C Bug fix

--- TrueMCOT.C
+++ TrueMCOT.C
@@ -1,10 +1,6 @@
-//g++-5 TrueMCOT.cpp -w `root-config --cflags` -I$FEDRA_ROOT/include -L$FEDRA_ROOT/lib -lEIO -lDataConversion -lEdb -lEbase -lEdr -lScan -lAlignment -lEmath -lEphys -lvt -lDataConversion `root-config --libs` -o MCOT
-//./MCOT
-
 #include <iostream>
 #include <vector>
 #include <cmath>
-#include <list>
 
 #include "TROOT.h"
 #include "TFile.h"
@@ -13,6 +9,12 @@
 #include "TH2.h"
 
 using namespace std;
+
+vector<int> eIDList;
+vector<int> eIDListFull;
+vector<int> discardedTracks;
+
+double dTLimit = 0.01;
 
 void TrueMCOT()
 {
@@ -53,11 +55,7 @@ void TrueMCOT()
   Float_t refTrTX, refTrTY, refTrT2, secTrTX, secTrTY;
   Float_t dTrTX; Float_t dTrTY;
 
-  int refIDEvent = 0, refFlag = 0, currentEID = 0;
-  vector<int> eIDList;
-  vector<int> eIDListFull;
-  vector<int> discardedTracks;
-  vector<int> selectedTracks;
+  int refIDEvent = 0, refFlag = 0, refParID = 0, currentEID = 0;
 
   for (int i = 0; i < TrueMCTree->GetEntriesFast(); i++)
   {
@@ -78,61 +76,63 @@ void TrueMCOT()
   {
     if(i%10 == 0) {cout << i << "/" << eSize << endl;}
 
-    multNum = count(eIDListFull.begin(), eIDListFull.end(), eIDList[i]);
+    multNum = count(eIDListFull.begin(), eIDListFull.end(), eIDList[i]); /*cout << "Mult: " << multNum << endl;*/
 
     index = 0;
     for(int j = 0; j < multNum; j++)
     {
       auto it = find(eIDListFull.begin() + index + 1, eIDListFull.end(), eIDList[i]);
-      if (it != eIDListFull.end() && !(find(discardedTracks.begin(), discardedTracks.end(), index) != discardedTracks.end()))
+      if (it != eIDListFull.end())
       {
-        index = it - eIDListFull.begin();
-        TrueMCTree->GetEntry(index);
-
-        refTrTX = MCTX->GetValue(); refTrTY = MCTY->GetValue(); refTrT2 = MCT2->GetValue();
-        refIDEvent = MCeID->GetValue();
-
-        secIndex = 0;
-        for(int k = 0; k < multNum; k++)
+        index = it - eIDListFull.begin(); //cout << "Index: " << index << endl;
+        if (find(discardedTracks.begin(), discardedTracks.end(), index) == discardedTracks.end())
         {
-          auto secIt = find(eIDListFull.begin() + secIndex + 1, eIDListFull.end(), eIDList[i]);
-          if(secIt != eIDListFull.end() && index != secIndex)
+          TrueMCTree->GetEntry(index);
+
+          refTrTX = MCTX->GetValue(); refTrTY = MCTY->GetValue(); refTrT2 = MCT2->GetValue();
+          refIDEvent = MCeID->GetValue(); refParID = MCParID->GetValue(); refFlag = MCFlag->GetValue();
+
+          //cout << "Primary: " << "Event: " << eIDListFull[index] << ", Track Num: " << index << endl;
+
+          secIndex = 0;
+          for(int k = 0; k < multNum; k++)
           {
-            if (!(find(discardedTracks.begin(), discardedTracks.end(), secIndex) != discardedTracks.end()))
+            auto secIt = find(eIDListFull.begin() + secIndex + 1, eIDListFull.end(), eIDList[i]);
+            if(secIt != eIDListFull.end())
             {
               secIndex = secIt - eIDListFull.begin();
-              TrueMCTree->GetEntry(secIndex);
+              if (find(discardedTracks.begin(), discardedTracks.end(), secIndex) == discardedTracks.end())
+              {
+                TrueMCTree->GetEntry(secIndex);
 
-              secTrTX = MCTX->GetValue();
-              secTrTY = MCTY->GetValue();
-              refFlag = MCFlag->GetValue();
+                //cout << "Secondary: " << "Event: " << eIDListFull[secIndex] << ", Track Num: " << secIndex << endl;
 
-              dTrTX = refTrTX - secTrTX;
-              dTrTY = refTrTY - secTrTY;
+                secTrTX = MCTX->GetValue();
+                secTrTY = MCTY->GetValue();
 
-              if ((dTrTX < 0.02 && dTrTX > -0.02 && dTrTY < 0.02 && dTrTY > -0.02) && refFlag != 222)
-              { 
-                /*
-                if (!(find(discardedTracks.begin(), discardedTracks.end(), secIndex) == discardedTracks.end()))
+                dTrTX = refTrTX - secTrTX;
+                dTrTY = refTrTY - secTrTY;
+
+                if (index != secIndex && (dTrTX < dTLimit && dTrTX > -dTLimit && dTrTY < dTLimit && dTrTY > -dTLimit))
                 {
                   discardedTracks.push_back(secIndex);
                 }
-                */
-                //eIDListFull.erase(next(eIDListFull.begin()));
-                discardedTracks.push_back(secIndex);
               }
-              //else{eIDListFull.erase(next(eIDListFull.begin()));}
             }
           }
-        }
 
-        tX = refTrTX; tY = refTrTY; t2 = refTrT2;
-        ev = refIDEvent; prID = MCParID->GetValue();
-        TrkTree->Fill();
+          if (/*refFlag == 222*/ refParID = 1)
+          {
+            tX = refTrTX; tY = refTrTY; t2 = refTrT2;
+            ev = refIDEvent; prID = refParID;
+            TrkTree->Fill();
+          }
+        }
       }
     }
   }
   monteCarloTrue->Close();
   TrkTree->Write();
   MCFile.Close();
+
 }
